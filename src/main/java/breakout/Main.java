fe90e3fd6ea86018a0d4f48c24a3e2f370a960c2
Owen Jennings <otj4@duke.edu>
2025-01-12 15:28:55 -0500

    added paddle with movement

--- src/main/java/breakout/Main.java
+++ src/main/java/breakout/Main.java
@@ -6,6 +6,7 @@ import javafx.animation.Timeline;
 import javafx.application.Application;
 import javafx.scene.Group;
 import javafx.scene.Scene;
+import javafx.scene.input.KeyCode;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 import javafx.util.Duration;
@@ -22,6 +23,7 @@ public class Main extends Application {
     public static final Color BACKGROUND_COLOR = new Color(0.1328, 0.1563, 0.1914, 1);
     public static final Color BALL_COLOR = new Color(0.9297, 0.9297, 0.9297, 1);
     public static final Color BLOCK_COLOR = new Color(0.2151, 0.2422, 0.2734, 1);
+    public static final Color PADDLE_COLOR = new Color(0.9297, 0.9297, 0.9297, 1);
     public static final int FRAMES_PER_SECOND = 60;
     public static final double SECOND_DELAY = 1.0 / FRAMES_PER_SECOND;
     public static final int WIDTH = 600;
@@ -30,6 +32,7 @@ public class Main extends Application {
     public Group root = new Group();
     public ArrayList<Ball> gameBalls = new ArrayList<>();
     public ArrayList<Block> gameBlocks = new ArrayList<>();
+    public Paddle gamePaddle;
 
     /**
      * Initialize what will be displayed.
@@ -56,6 +59,9 @@ public class Main extends Application {
             gameBlocks.add(block);
         }
 
+        gamePaddle = new Paddle(200, 750, 100, 10, 50, PADDLE_COLOR);
+        root.getChildren().add(gamePaddle);
+
         for (Ball ball : gameBalls) {
             root.getChildren().add(ball);
         }
@@ -64,6 +70,7 @@ public class Main extends Application {
         }
 
         Scene scene = new Scene(root, width, height, backgroundColor);
+        scene.setOnKeyPressed(e -> handleKeyInput(e.getCode()));
         return scene;
     }
 
@@ -91,9 +98,28 @@ public class Main extends Application {
         }
     }
 
-    /**
-     * Start the program, give complete control to JavaFX.
-     */
+    // What to do each time a key is pressed
+    private void handleKeyInput(KeyCode code) {
+        // NOTE new Java syntax that some prefer (but watch out for the many special cases!)
+        //   https://blog.jetbrains.com/idea/2019/02/java-12-and-intellij-idea/
+        switch (code) {
+            case RIGHT -> {
+                if (gamePaddle.canMoveRight(WIDTH)) {
+                    gamePaddle.move(1);
+                }
+            }
+            case LEFT -> {
+                if (gamePaddle.canMoveLeft()) {
+                    gamePaddle.move(-1);
+                }
+            }
+        }
+    }
+
+
+        /**
+         * Start the program, give complete control to JavaFX.
+         */
     public static void main(String[] args) {
         launch(args);
     }
---
+++ src/main/java/breakout/Paddle.java
@@ -0,0 +1,26 @@
+package breakout;
+
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
+
+public class Paddle extends Rectangle {
+  public double PADDLE_SPEED;
+
+  public Paddle(double x, double y, double width, double height, double speed, Color color) {
+    super(x, y, width, height);
+    PADDLE_SPEED = speed;
+    this.setFill(color);
+  }
+  public void move(double direction) {
+    this.setX(this.getX() + PADDLE_SPEED * direction);
+  }
+
+  public boolean canMoveLeft() {
+    return (this.getX() - PADDLE_SPEED >= 0);
+  }
+
+  public boolean canMoveRight(double width) {
+    return (this.getX() + this.getWidth() + PADDLE_SPEED <= width);
+  }
+
+}
