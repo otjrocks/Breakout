f06ccd3fc38831b0c06a5c6af9019fd731cbadd0
Owen Jennings <otj4@duke.edu>
2025-01-15 13:36:22 -0500

    Feature BREAK-17A: Began work on adding power-up block type and random power-ups within a level. Have not started powerup effects yet

--- src/main/java/breakout/Block.java
+++ src/main/java/breakout/Block.java
@@ -11,29 +11,33 @@ public class Block extends Group {
   private int health;
   private Text HEALTH_TEXT;
   private final double TEXT_SIZE;
+  private final String BLOCK_TYPE;
 
   public Block(int x, int y, String type, double size, int health, Color color, Color textColor) {
     TEXT_SIZE = size;
-    if (type.equals("square")) {
-      Rectangle rectangle = new Rectangle(size, size);
-      rectangle.setFill(color);
+    BLOCK_TYPE = type;
+    Rectangle rectangle = new Rectangle(size, size);
+    rectangle.setFill(color);
 
-      this.health = health;
-      HEALTH_TEXT = new Text(String.valueOf(health));
-      HEALTH_TEXT.setFill(textColor);
-      HEALTH_TEXT.setFont(new Font((size / 5)));
+    this.health = health;
+    HEALTH_TEXT = new Text(String.valueOf(health));
+    HEALTH_TEXT.setFill(textColor);
+    HEALTH_TEXT.setFont(new Font((size / 5)));
 
-      // I asked ChatGPT to help center text within a JavaFX rectangle
-      double textWidth = HEALTH_TEXT.getBoundsInLocal().getWidth();
-      double textHeight = HEALTH_TEXT.getBoundsInLocal().getHeight();
-      HEALTH_TEXT.setX((size - textWidth) / 2);
-      HEALTH_TEXT.setY((size + textHeight) / 2); // Adjust for baseline alignment
+    // I asked ChatGPT to help center text within a JavaFX rectangle
+    double textWidth = HEALTH_TEXT.getBoundsInLocal().getWidth();
+    double textHeight = HEALTH_TEXT.getBoundsInLocal().getHeight();
+    HEALTH_TEXT.setX((size - textWidth) / 2);
+    HEALTH_TEXT.setY((size + textHeight) / 2); // Adjust for baseline alignment
 
-      this.getChildren().addAll(rectangle, HEALTH_TEXT);
-      this.setLayoutX(x);
-      this.setLayoutY(y);
+    this.getChildren().addAll(rectangle, HEALTH_TEXT);
+    this.setLayoutX(x);
+    this.setLayoutY(y);
 
-    }
+  }
+
+  public String getBlockType() {
+    return BLOCK_TYPE;
   }
 
   public int getHealth() {
--- src/main/java/breakout/Level.java
+++ src/main/java/breakout/Level.java
@@ -6,6 +6,7 @@ import java.net.URISyntaxException;
 import java.net.URL;
 import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.Random;
 import java.util.Scanner;
 import javafx.scene.Group;
 import javafx.scene.paint.Color;
@@ -14,12 +15,17 @@ import javafx.scene.paint.Color;
 public class Level extends Group {
 
   public static final String LEVEL_FILE_PATH = "levels/";
+  public static final String[] POWER_UP_TYPES = new String[]{"addBall", "subtractBall", "scoreMultiplier", "blockDestroyer"};
+  public static final Color[] POWER_UP_COLORS = new Color[]{Color.LIGHTBLUE, Color.RED, Color.LIGHTGREEN, Color.LIGHTYELLOW};
+  public static final int POWER_UP_PROBABILITY = 10;  // with probability 1/X place a power-up in an empty space
   private final int SCREEN_WIDTH;
   private final int SCREEN_HEIGHT;
+  private final int BOTTOM_OFFSET = 2;
   private final int BLOCK_SIZE;
   private final Color BLOCK_COLOR;
   private final Color TEXT_COLOR;
   private final ArrayList<Block> blocks;
+  private final Random random;
 
   public Level(int screenWidth, int screenHeight, int blockSize, Color blockColor,
       Color textColor) {
@@ -29,6 +35,7 @@ public class Level extends Group {
     BLOCK_COLOR = blockColor;
     TEXT_COLOR = textColor;
     blocks = new ArrayList<>();
+    random = new Random();
   }
 
   private Scanner getScanner(int levelNumber) throws Exception {
@@ -57,19 +64,31 @@ public class Level extends Group {
   private void createLevel(int levelNumber) throws Exception {
     blocks.clear();
     Scanner scanner = getScanner(levelNumber);
-    for (int j = 0; j < SCREEN_HEIGHT / BLOCK_SIZE - 1; j++) {
+    for (int j = 0; j < SCREEN_HEIGHT / BLOCK_SIZE - BOTTOM_OFFSET; j++) {
       for (int i = 0; i < SCREEN_WIDTH / BLOCK_SIZE; i++) {
         if (scanner.hasNextInt()) {
           int nextInt = scanner.nextInt();
           if (nextInt > 0) {
-            Block block = new Block(i * BLOCK_SIZE, j * BLOCK_SIZE, "square", BLOCK_SIZE, nextInt,
+            Block block = new Block(i * BLOCK_SIZE, j * BLOCK_SIZE, "default", BLOCK_SIZE, nextInt,
                 BLOCK_COLOR, TEXT_COLOR);
             blocks.add(block);
+          } else {
+            createPowerups(i, j);
           }
         } else {
           throw new Exception("Level file is missing blocks!");
         }
       }
+    }
+  }
+
+  private void createPowerups(int i, int j ) {
+    boolean shouldAddPowerup = (random.nextInt(POWER_UP_PROBABILITY) == 0);
+    if (shouldAddPowerup) {
+      int powerUpIndex = random.nextInt(POWER_UP_TYPES.length);
+      String powerUpType = POWER_UP_TYPES[powerUpIndex];
+      Block block = new Block(i * BLOCK_SIZE, j * BLOCK_SIZE, powerUpType, BLOCK_SIZE, 1, POWER_UP_COLORS[powerUpIndex], TEXT_COLOR); // for now all powerups have 1 health
+      blocks.add(block);
     }
   }
 
