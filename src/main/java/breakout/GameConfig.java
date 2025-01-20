3a66d66922e6f17ee38d7bcf9237f29d0e6a11ac
Owen Jennings <otj4@duke.edu>
2025-01-20 15:06:32 -0500

    Doc DESIGN.md: updated the DESIGN.md document to explain my design decisions and deviations from the original plan document

--- src/main/java/breakout/GameConfig.java
+++ src/main/java/breakout/GameConfig.java
@@ -32,7 +32,7 @@ public class GameConfig {
   public static final int BALL_RADIUS = 5;
   public static final int BALL_SPEED = 320;
   public static final int INITIAL_NUM_LIVES = 5;
-  public static final double INITIAL_PADDLE_WIDTH = 150;
+  public static final double INITIAL_PADDLE_WIDTH = 180;
   public static final int PADDLE_SPEED = 8;
   public static final int SHOOTER_LENGTH = 100;
   public static final double MYSTERY_BLOCK_DISPLAY_TEXT_TIME = 3;
